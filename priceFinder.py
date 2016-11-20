9f7d69fd2ba0ee533e006cbd531c889ea363c678
Travis Pootoolal <tpootool@mail.uoguelph.ca>
2016-11-19 21:54:39 -0500

    Cleaned up the code a bit

--- priceFinder.py
+++ priceFinder.py
@@ -4,33 +4,30 @@ import numpy as np
 
 def main():
     # These are the amounts of items bought. Algorithm uses these
-    amounts = [4, 3, 7]
+    # amounts = np.array([random.randint(0,10), random.randint(0,10), random.randint(0,100)])
+    amounts = [random.randint(1,10), random.randint(1,10), random.randint(1,100)]
 
     # These are the actual prices of the items. Algorithm does not use these
-    prices = [35,48,52]
+    # prices = np.array([random.randint(0,100),random.randint(0,100),random.randint(0,100)])
+    prices = [random.randint(1,100),random.randint(1,100),random.randint(1,100)]
+
     # This is the ground truth, algorithm uses this
     totalTruePrice = 0
 
     learningRate = 1
     for i in range (0,3):
         totalTruePrice += amounts[i]*prices[i]
-
     totalTruePrice = (totalTruePrice)
-    # totalTruePrice = sigmoid(totalTruePrice)
-    #
-    # totalTruePrice += amounts[0:3] * prices[0:3]
-    # print totalTruePrice
 
     # Begin by initially randomizing weights and learned prices
-    # weights = [34,79,10]
-    weights = [random.random(),random.random(),random.random()]
+    weights = np.array([random.random(),random.random(),random.random()])
+    # weights = [random.random(),random.random(),random.random()]
+    # learnedPrices = np.array([0,0,0])
     learnedPrices = [0,0,0]
-    dErrdWeight = [0, 0, 0]
+    # dErrdWeight = np.array([0,0,0])
+    dErrdWeight = [0,0,0]
 
-    totalLearnedPrice = 0
-
-    prevError = 5
-    error = 6
+    error = 1
     while (error > .000001):
         totalLearnedPrice = 0
 
@@ -40,49 +37,30 @@ def main():
 
         totalLearnedPrice = (totalLearnedPrice)
         # totalLearnedPrice = sigmoid(totalLearnedPrice)
-        print weights
-        # print dErrdWeight
 
-        # print totalLearnedPrice
-        # print learnedPrices
-
-        prevError = error
+        # Compute the error given a mean squared error function
         error = 0.5*math.pow((totalTruePrice - totalLearnedPrice),2)
 
+        # The derivative of the error with respect to the value passed to it
         dErrdOutput = (totalTruePrice - totalLearnedPrice) * (-1)
-        # dOutputdWeight = amounts
 
         for i in xrange (0,3):
-            # dErrdWeight[i] = weights[i] * dErrdOutput
+            # The derivative of the error with respect to the 'i'th weight
             dErrdWeight[i] = amounts[i] * dErrdOutput
 
-            # weights[i] -= (learningRate*dErrdWeight[i])
-            weights[i] -= (learningRate*dErrdWeight[i])/1000
+            # weights[i] -= math.tanh(learningRate*dErrdWeight[i])
+            weights[i] -= (learningRate*dErrdWeight[i])/10000
 
-
-        print totalLearnedPrice
-
-        # print dErrdWeight
-        # print weights
-        # print totalLearnedPrice
-        # print dErrdOutput
-        # print dOutputdWeight
-        # print error
+    print "------------------------------------------------"
+    print "The quantities of items purchased are:"
+    print amounts
+    print "The actual prices of items are:"
+    print prices
+    print "The estimated prices of the items are:"
+    print weights
+    print "------------------------------------------------"
 
 def sigmoid(x):
     return 1/(1*math.exp(-x))
 
-
 main()
-
-# X = np.array([ [0,0,1],[0,1,1],[1,0,1],[1,1,1] ])
-# y = np.array([[0,1,1,0]]).T
-# syn0 = 2*np.random.random((3,4)) - 1
-# syn1 = 2*np.random.random((4,1)) - 1
-# for j in xrange(60000):
-#     l1 = 1/(1+np.exp(-(np.dot(X,syn0))))
-#     l2 = 1/(1+np.exp(-(np.dot(l1,syn1))))
-#     l2_delta = (y - l2)*(l2*(1-l2))
-#     l1_delta = l2_delta.dot(syn1.T) * (l1 * (1-l1))
-#     syn1 += l1.T.dot(l2_delta)
-#     syn0 += X.T.dot(l1_delta)
